c1237f544742a779812e95cc814ce8d44a287a18
isra-bouzidi <b.isra2004@gmail.com>
2025-09-30 18:43:36 +0100

    create navigation-bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PmaceOrder from './pages/PlaceOrder/PmaceOrder'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
-      
+      <Routes>
+        < Route path='/' element={<Home/>} />
+        < Route path='/cart' element={<Cart/>} />
+        < Route path='/order' element={<PmaceOrder/>} />
+      </Routes>
     </div>
   )
 }
